c3d880a97141cbb4383abf8730ac8f6c9d06ad4d
grantkee <grant.d.kee@gmail.com>
2020-07-01 08:49:25 -0500

    added discount conditions for passengers 65+

--- TransitCalculator.java
+++ TransitCalculator.java
@@ -7,6 +7,7 @@ public class TransitCalculator {
   int numOfDays;
   int numOfRides;
   int age;
+  boolean discount = false;
   String[] rideType = {"pay-per-ride", "7-day Unlimited", "30-day Unlimited"};
   double[] rideCost =  {2.75, 33.00, 127.00};
   double[] discountCost = {1.35, 16.50, 63.50};
@@ -17,10 +18,15 @@ public class TransitCalculator {
     numOfDays = days;
     numOfRides = rides;
     age = ageOfRider;
+    if(age >= 65){
+      discount = true;
+    }
   }
 
+  //cost per single ride
   public double singleRidePrice(){
-    double totalCost = rideCost[0]*numOfRides;
+    double[] cost = discount ? discountCost : rideCost;
+    double totalCost = cost[0]*numOfRides;
     totalCosts.add(totalCost);
     System.out.println("total cost for pay-per-ride: $" + totalCost);
     return totalCost / numOfRides;
@@ -28,18 +34,21 @@ public class TransitCalculator {
 
   //cost per ride using 7-day pass
   public double unlimited7Price(){
+    double[] cost = discount ? discountCost : rideCost;
     int numberOfPasses = numOfDays / 7 + (numOfDays % 7 == 0 ? 0 : 1);
     System.out.println("number of 7-day passes: " + numberOfPasses);
-    double totalCost = rideCost[1] * numberOfPasses;
+    double totalCost = cost[1] * numberOfPasses;
     totalCosts.add(totalCost);
     System.out.println("total cost for 7-day option: " + totalCost);
     return totalCost / numOfRides;
   }
 
+  //cost per ride using 30-day pass
   public double unlimited30Price(){
+    double[] cost = discount ? discountCost : rideCost;
     int numberOfPasses = numOfDays / 30 + (numOfDays % 30 == 0 ? 0 : 1);
     System.out.println("number of 30-day passes: " + numberOfPasses);
-    double totalCost = numberOfPasses * rideCost[2];
+    double totalCost = numberOfPasses * cost[2];
     totalCosts.add(totalCost);
     System.out.println("total cost for 30-day option: " + totalCost);
     return totalCost / numOfRides;
@@ -70,8 +79,6 @@ public class TransitCalculator {
 
     //convert lowest to two decimal places
     double lowestPrice = Math.round(lowest*100.0)/100.0;
-
-    //return options
 
     return "You should get the "+rideType[index]+" option at $"+lowestPrice+" per ride, which comes out to a total of $"+totalCosts.get(index)+".";
   }
